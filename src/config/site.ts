7383a36121ba44abf9e1c8e9b688de4a08ceb54c
jcarpio <jose.carpio@gmail.com>
2025-03-18 16:48:18 +0100

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,23 +4,23 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  title: "Fun & Pro AI Headshots Generator | HeadShots.fun",
-  name: "HeadShots.fun",
+  title: "Fun & Pro AI Headshots Generator | DreamBez.com",
+  name: "DreamBez.com",
   description:
     "Create stunning fun and professional headshots with AI at HeadShots.fun. Fast, affordable, and personalized AI-powered headshot generator. Perfect for LinkedIn, resumes, and social media.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
     twitter: "https://twitter.com/ullr_ai",
-    github: "https://github.com/ullrai/HeadShots.fun",
+    github: "https://github.com/jcarpio/dreambez",
   },
-  mailSupport: "support@headshots.fun",
+  mailSupport: "support@enkire.com",
   structuredData: {
     "@context": "https://schema.org",
     "@type": "WebSite",
-    "name": "HeadShots.fun",
+    "name": "DreamBez.com",
     "url": site_url,
-    "description": "Create stunning fun and professional headshots with AI at HeadShots.fun. Fast, affordable, and personalized AI-powered headshot generator. Perfect for LinkedIn, resumes, and social media.",
+    "description": "Create stunning fun and professional headshots with AI at DreamBez.com. Fast, affordable, and personalized AI-powered headshot generator. Perfect for LinkedIn, resumes, and social media.",
     "potentialAction": {
       "@type": "SearchAction",
       "target": `${site_url}/search?q={search_term_string}`,
@@ -32,10 +32,10 @@ export const siteConfig: SiteConfig = {
   ],
   authors: [
     {
-      name: "UllrAI",
+      name: "Enkire",
     },
   ],
-  creator: "UllrAI",
+  creator: "Enkire",
   twitterCreator: "@UllrAI",
   icons: "/favicon.ico",
   manifest: `${site_url}/site.webmanifest`,
